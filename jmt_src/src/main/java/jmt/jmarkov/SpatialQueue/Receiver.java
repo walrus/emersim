4a8f3deb37262d21be63f103a69efab16135c5e3
Daniel Clay <dclay.daniel@gmail.com>
2016-10-27 14:20:48 +0100

    Add skeleton methods to Receiver class

--- jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Receiver.java
+++ jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Receiver.java
@@ -4,4 +4,28 @@ package jmt.jmarkov.SpatialQueue;
  * Receivers handle requests from Senders
  */
 public class Receiver {
+
+    //Given a (newly arrived) Request, add it to the queue.
+    //This should be overridden to implement different behaviours
+    public void handle_request(Request request) {
+        //TODO: implement
+    }
+
+    // Given a Request object, calculate the response time
+    public double calculate_response_time(Request request) {
+        //TODO: implement
+        return 1.0;
+    }
+
+    //Given the a request, respond to it.
+    public void serve_request(Request request) {
+        //TODO: implement
+    }
+
+    //Find the next request in the queue. This should
+    //be overridden to implement different behaviours
+    public Request get_next_request() {
+        //TODO: implement
+        return null;
+    }
 }
