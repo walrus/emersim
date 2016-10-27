89d3f31a8bef992d3f6f7ddaea064e21a7d8b4b3
Daniel Clay <dclay.daniel@gmail.com>
2016-10-27 14:28:14 +0100

    Add skeleton methods to Sender class

--- jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Sender.java
+++ jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Sender.java
@@ -4,4 +4,24 @@ package jmt.jmarkov.SpatialQueue;
  * Senders send requests to Receivers.
  */
 public class Sender {
+
+    private Location location;
+    //How many requests the Sender should send
+    private int requests_to_send;
+
+    //Return the Sender's location
+    public Location get_location() {
+        //TODO: implement
+        return location;
+    }
+
+    //Send a variable number of requests to the Receiver
+    public void send_requests() {
+        //TODO: implement
+    }
+
+    //Send a request to the Receiver
+    public void make_request() {
+        //TODO: implement
+    }
 }
