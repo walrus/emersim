06843a63c93a63861b0b391f97247f61ae956470
Adam Quick <adam.quick@icloud.com>
2016-11-02 22:30:08 +0000

    Moved client/receiver management into respective classes

--- jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Map/ClientEntity.java
+++ jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Map/ClientEntity.java
@@ -1,19 +1,113 @@
 package jmt.jmarkov.SpatialQueue.Map;
 
-import com.teamdev.jxmaps.InfoWindow;
-import com.teamdev.jxmaps.Marker;
-import com.teamdev.jxmaps.Polygon;
+import com.teamdev.jxmaps.*;
 
-import static jmt.jmarkov.SpatialQueue.Map.MapConfig.clientRegions;
-import static jmt.jmarkov.SpatialQueue.Map.MapConfig.receiverMarkers;
+import javax.swing.*;
+import java.util.LinkedList;
+
+import static jmt.jmarkov.SpatialQueue.Map.MapConfig.*;
 
 class ClientEntity implements Entity {
     private Polygon polygon;
     private InfoWindow infoWindow;
+    private Marker marker;
+    private LinkedList<LatLng> path = new LinkedList<>();
+    private Polyline areaPeri;
 
-    ClientEntity(Polygon polygon, InfoWindow infoWindow) {
-        this.polygon = polygon;
-        this.infoWindow = infoWindow;
+    ClientEntity(MouseEvent mouseEvent) {
+        // Add first point to path
+        path.add(mouseEvent.latLng());
+        LatLng[] pathArray = new LatLng[path.size()];
+
+        areaPeri = new Polyline(map);
+        // Creating a polyline options object
+        PolylineOptions options = new PolylineOptions();
+        // Setting geodesic property value
+        options.setGeodesic(true);
+        // Setting stroke color value
+        options.setStrokeColor("#FF0000");
+        // Setting stroke opacity value
+        options.setStrokeOpacity(1.0);
+        // Setting stroke weight value
+        options.setStrokeWeight(2.0);
+        // Applying options to the polyline
+        areaPeri.setOptions(options);
+
+        // Set current perimeter
+        areaPeri.setPath(path.toArray(pathArray));
+
+        marker = new Marker(map);
+        marker.setPosition(mouseEvent.latLng());
+        marker.addEventListener("click", new MapMouseEvent() {
+            @Override
+            public void onEvent(MouseEvent mouseEvent) {
+                addPointToArea(mouseEvent);
+                finaliseArea();
+            }
+        });
+    }
+
+    void addPointToArea(MouseEvent mouseEvent) {
+        path.add(mouseEvent.latLng());
+        LatLng[] pathArray = new LatLng[path.size()];
+        areaPeri.setPath(path.toArray(pathArray));
+    }
+
+    private void finaliseArea() {
+        if (path.size() < 3) {
+            // Error, area must have >= 3 vertices
+            // TODO: Display error dialog
+        } else {
+            areaPeri.setVisible(false);
+            polygon = new Polygon(map);
+            // Initializing the polygon with the created path
+            LatLng[] pathArray = new LatLng[path.size()];
+            polygon.setPath(path.toArray(pathArray));
+            // Creating a polyline options object
+            PolygonOptions options = new PolygonOptions();
+            // Setting fill color value
+            options.setFillColor("#FF0000");
+            // Setting fill opacity value
+            options.setFillOpacity(0.35);
+            // Setting stroke color value
+            options.setStrokeColor("#FF0000");
+            // Setting stroke opacity value
+            options.setStrokeOpacity(0.8);
+            // Setting stroke weight value
+            options.setStrokeWeight(2.0);
+            // Applying options to the polygon
+            polygon.setOptions(options);
+
+            // Creating an information window
+            infoWindow = new InfoWindow(map);
+            // Putting the address and location to the content of the information window
+            infoWindow.setContent("<b>Client region #" + clientRegions.size() + "</b>");
+
+            // Moving the information window to the result location
+            infoWindow.setPosition(polygon.getPaths()[0][0]);
+            // Showing of the information window
+            infoWindow.open(map, polygon.getPaths()[0][0]);
+            polygon.addEventListener("click", new MapMouseEvent() {
+                @Override
+                public void onEvent(MouseEvent mouseEvent) {
+                    infoWindow.open(map, polygon.getPaths()[0][0]);
+                }
+            });
+            final Entity entity = this;
+            polygon.addEventListener("rightclick", new MapMouseEvent() {
+                @Override
+                public void onEvent(MouseEvent mouseEvent) {
+                    new MapEntityOptionsDialog(entity);
+                }
+            });
+
+            // Remove first vertex marker
+            marker.setVisible(false);
+            // Store area for use in simulation
+            clientRegions.add(polygon);
+            areaBeingDrawn = null;
+            drawingInProgess = false;
+        }
     }
 
     @Override
--- jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Map/MapConfig.java
+++ jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Map/MapConfig.java
@@ -17,9 +17,11 @@ public class MapConfig extends MapView {
 
     private static final String INITIAL_LOCATION = "Imperial College London, SW7 2AZ";
     private OptionsWindow optionsWindow;
-    private boolean placeMarker;
-    private boolean placeAreaVertex;
-    private Map map;
+    private boolean placeMarker = false;
+    private boolean drawNewArea = false;
+    static boolean drawingInProgess = false;
+    static ClientEntity areaBeingDrawn;
+    static Map map;
     static LinkedList<Polygon> clientRegions = new LinkedList<>();
     static LinkedList<Marker> receiverMarkers = new LinkedList<>();
 
@@ -49,33 +51,8 @@ public class MapConfig extends MapView {
                     @Override
                     public void onEvent(MouseEvent mouseEvent) {
                         if (placeMarker) {
-                            // Creating a new marker
-                            final Marker marker = new Marker(map);
-                            // Move marker to the position where user clicked
-                            marker.setPosition(mouseEvent.latLng());
-                            // Creating an information window
-                            final InfoWindow infoWindow = new InfoWindow(map);
-                            // Putting the address and location to the content of the information window
-                            infoWindow.setContent("<b>Receiver #" + receiverMarkers.size() + "</b>");
-                            // Moving the information window to the result location
-                            infoWindow.setPosition(marker.getPosition());
-                            // Showing of the information window
-                            infoWindow.open(map, marker);
-                            final ReceiverEntity entity = new ReceiverEntity(marker, infoWindow);
-                            marker.addEventListener("click", new MapMouseEvent() {
-                                @Override
-                                public void onEvent(MouseEvent mouseEvent) {
-                                    infoWindow.open(map, marker);
-                                }
-                            });
-                            marker.addEventListener("rightclick", new MapMouseEvent() {
-                                @Override
-                                public void onEvent(MouseEvent mouseEvent) {
-                                    new MapEntityOptionsDialog(entity);
-                                }
-                            });
                             placeMarker = false;
-                            receiverMarkers.add(marker);
+                            new ReceiverEntity(mouseEvent);
                         }
                     }
                 });
@@ -84,13 +61,18 @@ public class MapConfig extends MapView {
                 map.addEventListener("click", new MapMouseEvent() {
                     @Override
                     public void onEvent(MouseEvent mouseEvent) {
-                        if (placeAreaVertex)
-                            addPointToArea(mouseEvent);
+                        if (drawNewArea) {
+                            drawNewArea = false;
+                            areaBeingDrawn = new ClientEntity(mouseEvent);
+                            drawingInProgess = true;
+                        }
+                        if (drawingInProgess) {
+                            areaBeingDrawn.addPointToArea(mouseEvent);
+                        }
                     }
                 });
             }
         });
-
     }
 
     @Override
@@ -206,99 +188,12 @@ public class MapConfig extends MapView {
         });
     }
 
-    private Polyline pathLine;
-    private LinkedList<LatLng> path = new LinkedList<>();
-    private void addPointToArea(MouseEvent mouseEvent) {
-        path.add(mouseEvent.latLng());
-        LatLng[] pathArray = new LatLng[path.size()];
-        pathLine.setPath(path.toArray(pathArray));
-        if (path.size() == 1) {
-            final Marker marker = new Marker(map);
-            marker.setPosition(mouseEvent.latLng());
-            marker.addEventListener("click", new MapMouseEvent() {
-                @Override
-                public void onEvent(MouseEvent mouseEvent) {
-                    if (path.size() < 3) {
-                        // Error, area must have >= 3 vertices
-                        // TODO: Display error dialog
-                        JDialog errorDialog = new JDialog();
-
-                    } else {
-                        placeAreaVertex = false;
-                        pathLine.setVisible(false);
-                        final Polygon polygon = new Polygon(map);
-                        // Initializing the polygon with the created path
-                        LatLng[] pathArray = new LatLng[path.size()];
-                        polygon.setPath(path.toArray(pathArray));
-                        // Creating a polyline options object
-                        PolygonOptions options = new PolygonOptions();
-                        // Setting fill color value
-                        options.setFillColor("#FF0000");
-                        // Setting fill opacity value
-                        options.setFillOpacity(0.35);
-                        // Setting stroke color value
-                        options.setStrokeColor("#FF0000");
-                        // Setting stroke opacity value
-                        options.setStrokeOpacity(0.8);
-                        // Setting stroke weight value
-                        options.setStrokeWeight(2.0);
-                        // Applying options to the polygon
-                        polygon.setOptions(options);
-
-                        // Creating an information window
-                        final InfoWindow infoWindow = new InfoWindow(map);
-                        // Putting the address and location to the content of the information window
-                        infoWindow.setContent("<b>Client region #" + clientRegions.size() + "</b>");
-
-                        // Moving the information window to the result location
-                        infoWindow.setPosition(polygon.getPaths()[0][0]);
-                        // Showing of the information window
-                        infoWindow.open(map, polygon.getPaths()[0][0]);
-                        polygon.addEventListener("click", new MapMouseEvent() {
-                            @Override
-                            public void onEvent(MouseEvent mouseEvent) {
-                                infoWindow.open(map, polygon.getPaths()[0][0]);
-                            }
-                        });
-                        final ClientEntity entity = new ClientEntity(polygon, infoWindow);
-                        polygon.addEventListener("rightclick", new MapMouseEvent() {
-                            @Override
-                            public void onEvent(MouseEvent mouseEvent) {
-                                new MapEntityOptionsDialog(entity);
-                            }
-                        });
-
-                        // Delete current path
-                        path = new LinkedList<>();
-                        // Remove first vertex marker
-                        marker.setVisible(false);
-                        // Store area for use in simulation
-                        clientRegions.add(polygon);
-                    }
-                }
-            });
-        }
-    }
-
     public void toggleMarkerPlacement() {
         placeMarker = true;
     }
 
     public void toggleAreaPlacement() {
-        placeAreaVertex = true;
-        pathLine = new Polyline(map);
-        // Creating a polyline options object
-        PolylineOptions options = new PolylineOptions();
-        // Setting geodesic property value
-        options.setGeodesic(true);
-        // Setting stroke color value
-        options.setStrokeColor("#FF0000");
-        // Setting stroke opacity value
-        options.setStrokeOpacity(1.0);
-        // Setting stroke weight value
-        options.setStrokeWeight(2.0);
-        // Applying options to the polyline
-        pathLine.setOptions(options);
+        drawNewArea = true;
     }
 
     public Location translateCoordinate(LatLng location) {
