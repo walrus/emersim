5aef8c16606ba8068d6e4484544bf588f2e0119a
Daniel Clay <dclay.daniel@gmail.com>
2016-11-08 11:46:02 +0000

    Remove Request's dependency on Job

--- jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Simulation/Request.java
+++ jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Simulation/Request.java
@@ -1,17 +1,48 @@
 package jmt.jmarkov.SpatialQueue.Simulation;
 
 import jmt.jmarkov.Job;
+import jmt.jmarkov.Queues.Processor;
 import jmt.jmarkov.SpatialQueue.Simulation.Sender;
 
+import static jmt.jmarkov.SpatialQueue.Simulation.Request.RequestState.BEING_SERVED;
+import static jmt.jmarkov.SpatialQueue.Simulation.Request.RequestState.FINISHED;
+import static jmt.jmarkov.SpatialQueue.Simulation.Request.RequestState.IN_QUEUE;
+
 /**
- * Requests are sent by Senders to Receivers
+ * Requests are sent by Senders to Receivers. They fulfil the same function as Jobs in JMCH.
  */
-public class Request extends Job{
+public class Request{
     private Sender sender;
 
-    public Request(int jobId, double time, Sender sender){
-        super(jobId, time);
+    private int requestId;
+
+    private double creationTime;
+
+    private double startServiceTime;
+
+    private double finishServiceTime;
+
+    private double nextEventTime; // Needed?
+
+    private RequestState currentState;
+
+    enum RequestState {IN_QUEUE, BEING_SERVED, FINISHED}
+
+    public Request(int requestId, double time, Sender sender){
+        this.requestId = requestId;
+        this.creationTime = time;
+        this.currentState = IN_QUEUE;
         this.sender = sender;
+    }
+
+    public void serve(double time){
+        this.currentState = BEING_SERVED;
+        this.startServiceTime = time;
+    }
+
+    public void finishServing(double time){
+        this.currentState = FINISHED;
+        this.finishServiceTime = time;
     }
 
     public Sender getSender() {
