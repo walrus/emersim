6555cac16a9d848e9dca6a131c8798ebc6e445e5
Loretta <yuting.lin16@gmail.com>
2016-11-15 15:41:18 +0000

    Add test for random request generation

---
+++ jmt_src/src/test/java/jmt/jmarkov/SpatialQueue/RandomTimeTest.java
@@ -0,0 +1,13 @@
+package jmt.jmarkov.SpatialQueue;
+
+import org.junit.Test;
+
+/**
+ * Created by risingpo on 10/11/2016.
+ */
+public class RandomTimeTest {
+    @Test
+    public void canGenerateNextTimeWithPoissonDistribution() {
+
+    }
+}
