cc26352779d1285eb10c313518921c5cd9f6db10
Joshua Zeltser <joshua.zeltser@gmail.com>
2017-01-04 14:44:55 +0000

    Made the error message for invalid travel route more readable and look nicer

--- jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Map/APIErrorDialog.java
+++ jmt_src/src/main/java/jmt/jmarkov/SpatialQueue/Map/APIErrorDialog.java
@@ -1,5 +1,7 @@
 package jmt.jmarkov.SpatialQueue.Map;
 
+import jmt.jmarkov.SpatialQueue.Gui.SpatialQueueFrame;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -18,40 +20,13 @@ public class APIErrorDialog extends JFrame {
     }
 
     private void init() {
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-        JLabel errorLabel = new JLabel("A route could not be calculated for this journey.");
-        errorLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        mainPanel.add(errorLabel);
-        mainPanel.add(Box.createVerticalGlue());
 
-        JLabel errorLabel2 = new JLabel("Please choose another travel mode and try again.");
-        errorLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        mainPanel.add(errorLabel2);
-        mainPanel.add(Box.createVerticalGlue());
+        JOptionPane.showMessageDialog(this, "A route could not be calculated for this journey," +
+                "please choose another travel mode and try again.");
+        
+        this.pack();
+        this.setVisible(true);
 
-        JButton closeButton = new JButton("Close");
-        closeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
-        });
-        closeButton.setPreferredSize(new Dimension(90, 30));
-        closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        mainPanel.add(closeButton);
-
-        mainPanel.add(Box.createVerticalGlue());
-
-        add(mainPanel);
-        setTitle("Route not found");
-
-        Dimension d = new Dimension(600, 100);
-        setPreferredSize(d);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
     }
 }
